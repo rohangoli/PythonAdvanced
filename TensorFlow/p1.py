441a355076fb4873f9e01cd94d8f9cecd4bb5a9b
rogoli-tcs <rohanr27@gmail.com>
2018-05-07 10:02:12 +0530

    renamed p1.py to p1(intro2TF).py

--- TensorFlow/p1.py
+++
@@ -1,9 +0,0 @@
-## Hello program from Tensor Flow
-
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
