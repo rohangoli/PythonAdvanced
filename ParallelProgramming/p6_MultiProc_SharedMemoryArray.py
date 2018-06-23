411a760fcc0c463477c7dc0dedf2ae4a4c5cea6e
rogoli-tcs <rohanr27@gmail.com>
2018-06-23 23:06:39 +0530

    Using Shared Memory Value

--- ParallelProgramming/p6_MultiProc_SharedMemoryArray.py
+++ ParallelProgramming/p6_MultiProc_SharedMemoryArray.py
@@ -1,11 +1,12 @@
 import time
 import multiprocessing
 
-def calc_sqrt(numbers,result):
+def calc_sqrt(numbers,result,v):
+    v.value=5.67
     print("Calculate Square of Numbers:")
     for i,j in enumerate(numbers):
         result[i]=j*j
-    print("Inside Process:",result[:])
+    print("Inside Process:",result[:],v.value)
         
 
 if __name__ == '__main__':
@@ -13,11 +14,12 @@ if __name__ == '__main__':
     t=time.time()
 
     result=multiprocessing.Array('i',4)
-    p1=multiprocessing.Process(target=calc_sqrt,args=(arr,result))
+    v=multiprocessing.Value('d',0.0)
+    p1=multiprocessing.Process(target=calc_sqrt,args=(arr,result,v))
 
     p1.start()
 
     p1.join()
 
-    print("Outside Process:",result[:])
+    print("Outside Process:",result[:],v.value)
     print("Total time taken:",time.time()-t)
